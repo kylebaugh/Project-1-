932c7d0f9719af69fc81599470c5ac52409144b0
Zachary Sterman <zacharysterman1992@gmail.com>
2021-11-01 17:43:20 -0400

    deployment

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const axios = require('axios').default
+const path = require('path')
 
 app.use(express.json())
 app.use(cors())
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../main.js'))
+})
 
 
 const port = process.env.PORT || 4545
